8434bf0ff851dd7c7f4bc9df8f5b7b21fa69a919
babacandiaye <ndiaybabacarnjaay@gmail.com>
2024-02-17 15:04:06 +0000

    fix: middleware

--- internals/handlers/middleware.go
+++ internals/handlers/middleware.go
@@ -1,21 +1,33 @@
 package handlers
 
 import (
+	"fmt"
 	"forum/internals/utils"
 	"net/http"
 )
 
 func ErrorMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		found := false
+		foundpath, foundmethod := false, false
 		for _, route := range Routes {
 			if route.Path == r.URL.Path {
-				found = true
+				foundpath = true
+				for _, method := range route.Method {
+					if r.Method == method {
+						foundmethod = true
+					}
+				}
 			}
 		}
-		if !found {
+		if !foundpath {
 			w.WriteHeader(404)
 			utils.FileService("error.html", w, Err[404])
+			return
+		}
+		if !foundmethod {
+			fmt.Println("middleware")
+			w.WriteHeader(405)
+			utils.FileService("error.html", w, Err[405])
 			return
 		}
 		next.ServeHTTP(w, r)

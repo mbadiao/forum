0a322c9925775fbc084fa41bfb9ff83cad3d52bb
emalo <elie.jean.malo@gmail.com>
2024-02-19 12:16:58 +0000

    id check comment

--- internals/handlers/middleware.go
+++ internals/handlers/middleware.go
@@ -4,12 +4,20 @@ import (
 	"fmt"
 	"forum/internals/utils"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 func ErrorMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		foundpath, foundmethod := false, false
 		for _, route := range Routes {
+			if strings.HasPrefix(route.Path, "/comment") {
+				if !IdCheck(w, r) {
+					utils.FileService("error.html", w, Err[404])
+					return
+				}
+			}
 			if route.Path == r.URL.Path {
 				foundpath = true
 				for _, method := range route.Method {
@@ -32,4 +40,15 @@ func ErrorMiddleware(next http.Handler) http.Handler {
 		}
 		next.ServeHTTP(w, r)
 	})
+}
+
+func IdCheck(w http.ResponseWriter, r *http.Request) bool {
+	if strings.HasPrefix(r.URL.Path, "/comment") {
+		idStr := r.URL.Query().Get("id")
+		_, err := strconv.Atoi(idStr)
+		if err != nil {
+			return false
+		}
+	}
+	return true
 }
